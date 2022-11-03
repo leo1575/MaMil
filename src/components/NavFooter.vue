f345e5e8d5a8cbf51669313d883e27ce262f17a3
leo <1312381263@qq.com>
2022-11-03 23:10:34 +0800

    项目目录结构设置

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:'NavFooter'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavHeader/>
+    <router-view></router-view>
+    <NavFooter/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
